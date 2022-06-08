777609db14b682eb6a2413f03d737d64a9f01ab8
AdrianaRoza <adriana.roza.aka@gmail.com>
2022-06-07 23:06:22 -0400

    start project end clean

---
+++ src/App.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import {v4 as uuidv4} from "uuid";
+
+import Tasks from "./Components/Tasks";
+import AddTask from "./Components/AddTask";
+import './App.css'
+
+const App = () => {
+  const [tasks, setTasks] = useState([
+    {
+        id: '1',
+        title: 'Estudar Programação',
+        completed: false,
+    },
+    {
+        id: '2',
+        title: 'Ler Livros',
+        completed: true,
+    },
+  ]);
+
+  const handleTaskClick = (taskId) => {
+      const newTasks = tasks.map((task) => {
+        if (task.id === taskId) return {...task, completed: !task.completed};
+        
+        return task;
+        });
+      };
+
+  const handleTaskAddition = (taskTitle) => {
+      const newTasks = [...tasks, {
+          title: taskTitle,
+          id: uuidv4(),
+          completed: false,
+      },
+    ];
+
+    setTasks(newTasks);
+
+  }
+
+  return (
+      <>
+          <div className="container">
+              <AddTask  handleTaskAddition={ handleTaskAddition}/>
+              <Tasks tasks={tasks} handleTaskClick = {handleTaskClick}/>
+          </div>
+      </>
+  );
+};
+
+export default App;
